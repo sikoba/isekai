8ace3406a883a50d1583482677ec55dd591722c6
Nemanja Boric <4burgos@gmail.com>
2018-12-17 23:20:54 +0100

    Add logger helper class
    This adds the helper class around Logger instance to be used
    inside the parser

--- src/parser.cr
+++ src/parser.cr
@@ -4,6 +4,29 @@ require "logger"
 module Isekai
     VERSION = "0.1.0"
 
+    # Simple wrapper around the logger's instance.
+    # Takes care of the Logger's setup and holds Logger instance
+    class Log
+      @@log = Logger.new(STDOUT)
+
+      # Setup the logger
+      # Parameters:
+      #     verbose = be verbose at the output
+      def self.setup (verbose = false)
+          if verbose
+              @@log.level = Logger::DEBUG
+          else
+              @@log.level = Logger::WARN
+          end
+      end
+
+      # Returns:
+      #     the logger instance
+      def self.log
+          @@log
+      end
+    end
+
     # Class that parses and transforms C code into internal state
     class CParser
         # Initialization method.
