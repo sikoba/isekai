b38e1b12624e7901f109759697c2b36cc10d95f6
Nemanja Boric <4burgos@gmail.com>
2018-12-22 23:21:21 +0100

    Add definition of internal types.
    This adds definition for all internal types: (unsigned) integer, array
    type, struct type, pointer type and the void type.

---
+++ spec/frontend_types_spec.cr
@@ -0,0 +1,15 @@
+require "spec"
+require "../src/frontend/types.cr"
+
+describe Isekai do
+    fields = [Isekai::StructField.new(Isekai::IntType.new(), "a"),
+              Isekai::StructField.new(Isekai::UnsignedType.new(), "b")]
+    my_struct = Isekai::StructType.new("my_struct", fields)
+
+    exp_size = (Isekai::IntType.new().sizeof + Isekai::UnsignedType.new().sizeof)
+    my_struct.sizeof.should eq exp_size
+
+    my_struct.offsetof("a").should eq 0
+    my_struct.offsetof("b").should eq my_struct.get_field("a").@type.sizeof
+end
+
