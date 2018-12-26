ca47e8ae429babf7e3519d75666a00c2decf5907
Nemanja Boric <nemanja@nemanjaboric.com>
2018-12-26 13:25:33 +0100

    Add parse_file_to_ast_tree method and parse the input file.
    This methods parses the input C file into the AST tree.

--- src/parser.cr
+++ src/parser.cr
@@ -30,6 +30,9 @@ module Isekai
 
     # Class that parses and transforms C code into internal state
     class CParser
+        # Root input cursor
+        @file_ast_cursor : Clang::Cursor
+
         # Initialization method.
         # Parameters:
         #   input_file = C file to read
@@ -39,6 +42,30 @@ module Isekai
         #   progress = print progress during processing
         def initialize (input_file : String, clang_args : String, @loop_sanity_limit : Int32,
                         @bit_width : Int32, @progress = false)
+            @ast_cursor = parse_file_to_ast_tree(input_file, clang_args)
+        end
+
+        # Reads and parses the input file. Returns the AST tree representation.
+        # Params:
+        #     input_file = source file to parse
+        #
+        # Returns:
+        #     cursor representing AST tree of the input file
+        #
+        def parse_file_to_ast_tree (input_file, clang_args) : Clang::Cursor
+            # Creates a clang's index. Index holds the state of the parser
+            # and it needs to be initialized before parsing
+            index = Clang::Index.new
+
+            # 1. Set default options
+            options = Clang::TranslationUnit.default_options
+
+            # 2. Load the file and get the translation unit
+            files = [Clang::UnsavedFile.new(input_file, File.read(input_file))]
+            tu = Clang::TranslationUnit.from_source(index, files, clang_args.split, options)
+
+            # 3. return the cursor
+            return tu.cursor
         end
     end
 end
