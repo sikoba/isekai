8e25571d2af15405cbce9d6e74facf0cb8e9d6fa
shdown <shdownnine@gmail.com>
2019-08-01 00:25:36 +0300

    tools/loops/phi.c: fix

--- tools/loops/phi.c
+++ tools/loops/phi.c
@@ -6,13 +6,28 @@ void outsource(struct Input *input, struct Output *output)
     int a = input->a;
     int b = input->b;
 #if ISEKAI_C_PARSER
-    if (a == 0) {
-        b = b + 1;
-    }
+
+#   define BEGIN   int tmp_ = 0; \
+                   if (a != 77) { \
+                       tmp_ = (b != 128); \
+                       b = b + 1; \
+                   } \
+                   if (tmp_) { \
+                       a = a + 1;
+
+#   define END     }
+
+    BEGIN
+        BEGIN
+            BEGIN
+            END
+        END
+    END
+
 #else
-    while (a || b++) {
+    while (a != 77 && b++ != 128) {
         a++;
     }
 #endif
-    output->x = input->a;
+    output->x = a;
 }
