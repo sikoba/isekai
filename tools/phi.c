75aa02ab93dcc1789b935d87e0977fd7568c8de5
shdown <shdownnine@gmail.com>
2019-07-31 21:45:20 +0300

    tools/phi.c: update

--- tools/phi.c
+++ tools/phi.c
@@ -2,6 +2,7 @@ struct Input {
     int a;
     int b;
     int c;
+    int A, B, C, D, E, F, G, H;
 };
 
 struct Output {
@@ -11,39 +12,40 @@ struct Output {
 void
 outsource(struct Input *input, struct Output *output)
 {
+#define I(Field_) input->Field_
 #if ISEKAI_C_PARSER
     if (input->a != 0) {
         if (input->b != 0) {
             if (input->c != 0) {
-                output->x = 1;
+                output->x = I(A);
             } else {
-                output->x = 2;
+                output->x = I(B);
             }
         } else {
             if (input->c != 0) {
-                output->x = 3;
+                output->x = I(C);
             } else {
-                output->x = 4;
+                output->x = I(D);
             }
         }
     } else {
         if (input->b != 0) {
             if (input->c != 0) {
-                output->x = 5;
+                output->x = I(E);
             } else {
-                output->x = 6;
+                output->x = I(F);
             }
         } else {
             if (input->c != 0) {
-                output->x = 7;
+                output->x = I(G);
             } else {
-                output->x = 8;
+                output->x = I(H);
             }
         }
     }
 #else
     output->x = input->a
-        ? (input->b ? (input->c ? 1 : 2) : (input->c ? 3 : 4))
-        : (input->b ? (input->c ? 5 : 6) : (input->c ? 7 : 8));
+        ? (input->b ? (input->c ? I(A) : I(B)) : (input->c ? I(C) : I(D)))
+        : (input->b ? (input->c ? I(E) : I(F)) : (input->c ? I(G) : I(H)));
 #endif
 }
