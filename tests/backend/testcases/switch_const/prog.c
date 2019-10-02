ec437bd32d4933a5aee61598f0da464984986d12
shdown <shdownnine@gmail.com>
2019-10-02 07:25:49 +0300

    tests/backend/testcases: add more

---
+++ tests/backend/testcases/switch_const/prog.c
@@ -0,0 +1,18 @@
+struct Input {
+    int a;
+};
+
+struct Output {
+    int x;
+};
+
+void outsource(struct Input *input, struct Output *output)
+{
+    int v = 4;
+    switch (v) {
+    case 1: output->x = 22; break;
+    case 4: output->x = 77; break;
+    case 9: output->x = 99; break;
+    default: output->x = input->a; break;
+    }
+}
